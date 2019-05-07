bad51d936d1bd68cab7c3c8d0aac8943a535839c
matthewtrahan <mtrahan920@sbcglobal.net>
2019-05-07 12:22:00 -0500

    fix reactstrap style

--- react-client/src/components/headerComponent/NavBar.jsx
+++ react-client/src/components/headerComponent/NavBar.jsx
@@ -13,17 +13,33 @@ import {
   DropdownMenu,
   DropdownItem } from 'reactstrap';
 
-class NavBar extends Component {
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div>
-        <Navbar id="navbar-override" color="dark" light expand="md">
+        <Navbar id="navbar-override" light expand="md">
           <NavbarBrand id="bootstrap-override" href="/">Matthew Trahan</NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/resources/MatthewTrahanResumePDF.pdf">Resumé</NavLink>
-            </NavItem>
-          </Nav>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem id="navitem-override">
+                <NavLink id="bootstrap-override" href="/resources/MatthewTrahanResumePDF.pdf">Resumé</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
       </div>
     )
